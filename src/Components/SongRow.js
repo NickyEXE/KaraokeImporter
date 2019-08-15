9e64c66ee26a74f28ef7cbf8e0a27aceb9b8f0c1
NickyEXE <dovern42@gmail.com>
2019-08-15 15:00:11 -0400

    full redesign of playlist page

--- src/Components/SongRow.js
+++ src/Components/SongRow.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 
 class SongRow extends Component {
 
-
+    doIExist = () => this.props.code && this.props.code !== "0"
 
     render(){
+        console.log(this.props.code)
+
+        const songCode = () => {
+            switch (this.props.code){
+                case "0":
+                    return "None"
+                    break;
+                case null:
+                    return <div className="loader"></div>
+                default:
+                    return this.props.code
+            }
+        }
 
         // <td><button onClick={() => this.props.goToSongEdit(this.props.id, this.props.playlistId)}>Edit</button></td>
 
         return (
             <div className="grid-container">
-                <div className="song-code">{this.props.code}</div>
+                <div className="song-code">{songCode()}</div>
                 <div className="spotify-song"><strong>{this.props.spotify_name}</strong> by {this.props.spotify_artist}</div>
-                <div className="karaoke-song">{this.props.code && "(As: " + this.props.title +" by " + this.props.artist +")"}</div>                 
+                <div className="karaoke-song">{this.doIExist() && "(As: " + this.props.title +" by " + this.props.artist +")"}</div>                 
             </div>
             )
     }
