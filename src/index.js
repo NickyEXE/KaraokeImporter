82d8fe3358a051235209febd5b9151003312d206
NickyEXE <dovern42@gmail.com>
2019-08-19 18:55:33 -0400

    making it a secure websocket connection

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 // import {Route} from 'react-router-dom/Route';
 
 
-const API_WS_ROOT = 'ws://serene-scrubland-24770.herokuapp.com/cable'
+const API_WS_ROOT = 'wss://serene-scrubland-24770.herokuapp.com/cable'
 
 
 ReactDOM.render(
