4284ad343c6ec322dac75fd63b31a798339c9a43
NickyEXE <dovern42@gmail.com>
2019-10-08 16:51:09 -0400

    added a dropdown navbar for mobile

--- src/Components/Navigator.js
+++ src/Components/Navigator.js
@@ -1,13 +1,24 @@
-    
-import React from 'react'
+import React, {Component} from 'react'
+class Navigator extends Component {
 
-const Navigator = (props) =>  {
-  return (
-    <center className="nav">
-      <div className="nav-button" onClick={props.goToNewPlaylist}>Create a New Playlist</div>
-      <div className="nav-button" onClick={props.goToPlaylists}>Go To Existing Playlists</div>
-    </center>
-  )
+  state={
+    expanded: false
+  }
+
+  onClick = () => this.setState(prevState => ({expanded: !prevState.expanded}))
+
+  expanded = () => "nav-button" + (this.state.expanded ? " verticalized" : "")
+
+  render(){
+    return (
+      // add clicked
+      <center className={this.state.expanded ? "nav selected" : "nav"}>
+        <div className={this.expanded()} onClick={this.props.goToPlaylists}>View Playlists</div>
+        <div className={this.expanded()} onClick={this.props.goToNewPlaylist}>Create a New Playlist</div>
+        <div className="dropdown" onClick={this.onClick}><img id="dropdown-icon" className={this.state.expanded ? "dropdown-icon clicked" : ""} src={require("../assets/dropdown.png")} alt="Nicky Singing"/></div>
+      </center>
+    )
+  }
 }
 
 
